275a71ff537d24603ced9f86c7e58ec2daf4cf85
Juan P. Tejo <juan.pablo.tejo@hotmail.com>
2022-10-20 15:23:33 -0300

    primera subida

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   
+    <?php
+
+        $numero=3;
+        $valor = 2;
+        echo $numero + $valor;
+
+        echo "<br>";
+
+        $nombre = "Juan";
+        $apellido = "Tejo";
+        echo $nombre . " " . $apellido;
+
+        echo "<br>";
+
+        //echo $_GET["id"];
+        if(isset($_GET["id"])){
+            echo $_GET["id"];}
+
+        $lista =[1,2,3,4,5,6,7,8,9,10];
+    ?>
+
+    <form method="GET" action="">
+        <input type="text" name="id">
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
